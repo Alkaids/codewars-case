92da2c4b12108b39f93665fb6e6309941664349a
jinxin0821 <jinxin.0821@bytedance.com>
2020-01-21 11:11:46 +0800

    chore: initial package

---
+++ typescript/learnMixin.ts
@@ -0,0 +1,11 @@
+// ref: https://www.codewars.com/kata/597277844998c50c9a000051/train/typescript
+
+export class Serializable {
+    serialize(): void {
+        // TODO:
+    }
+
+    deserialize(source: string): void {
+        // TODO:
+    }
+}
