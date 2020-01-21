8599066de846deb3a1e044f35dd2fadeb9f33e6d
jinxin0112 <m18508218948@163.com>
2020-01-21 15:07:55 +0800

    feat(typescript): cpt Serializable

--- typescript/src/learnMixin.ts
+++ typescript/src/learnMixin.ts
@@ -1,28 +1,11 @@
 // ref: https://www.codewars.com/kata/597277844998c50c9a000051/train/typescript
 
 export class Serializable {
-    stringfiyValue = (value: any): string => {
-        const t = typeof value;
-        if (t !== 'object' || t === null) {
-            return `"${String(value)}"`;
-        } else {
-            // todo 深度序列化
-            return '';
-        }
-    };
-
-    stringfiy = (pre: string, [key, value]: [string, string], index: number): string => {
-        const itemStr = `"${key}": ${this.stringfiyValue(value)}`;
-        pre += itemStr;
-        index === Object.keys(this).length - 1 && (pre += '}');
-        return pre;
-    };
-
     serialize(): string {
-        return Object.entries(this).reduce(this.stringfiy, '{');
+        return JSON.stringify(this);
     }
 
     deserialize(source: string): void {
-        // TODO:
+        Object.assign(this, JSON.parse(source));
     }
 }
